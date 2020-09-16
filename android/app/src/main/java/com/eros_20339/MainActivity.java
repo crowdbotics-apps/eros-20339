0fb239ae2dbf34d332f0f8780b7d8b49db08abdf
Crowdbotics <team@crowdbotics.com>
2020-09-16 17:55:56 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/eros_20339/MainActivity.java
@@ -0,0 +1,15 @@
+package com.eros_20339;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "eros_20339";
+    }
+}
